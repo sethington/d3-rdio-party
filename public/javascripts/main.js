a6a0381687cbf51303c2202dde9d6aacaadf566c
Seth Steele <sethsteele@gmail.com>
2013-11-17 18:46:03 -0600

    adding rendering method and album switching

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,6 +9,11 @@ $(document).ready(function(){
         $(this).rdio().play('a2660305');
     });
 
+    $("#albums").change(function(e){
+    	var song_val = $(e.currentTarget).val();
+    	rdioObj.rdio().play(song_val);
+    });
+
 	rdioObj.bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
 		if (playingTrack) {
 			rdioObj.rdio().startFrequencyAnalyzer({
@@ -45,23 +50,89 @@ $(document).ready(function(){
 		rdioObj.rdio().previous();
 	});
 
-	var base_data = [0,0,0,0,0,0,0,0];
-
 	var container = d3.select("#party_box svg")
 		.attr("width", "100%")
 		.attr("height", 500);
-		
-	var circles = container.selectAll("circle")
-		.data(base_data)
-		.enter()
-		.append("circle");
 
+	var visualizer = Visualizer({
+		"container": container,
+		"mode": "circles"
+	});
+	
 	rdioObj.bind("updateFrequencyData.rdio", function(e,data){
 		data = data.split(",");
 
-		circles
-			.data(data)
-			.enter();
+		// pass data to renderer
+		visualizer.render(data);
+	});
+
+	$("#renderer").change(function(e){
+		visualizer.setRenderer($(e.currentTarget).val());
+	});
+});
+
+var Visualizer = function(options){
+	var self = this;
+	
+	var base_data = [0,0,0,0,0,0,0,0],
+		container = options.container;
+
+	self.api = {
+		'setRenderer': function(renderer){
+			options.mode = renderer;
+			$(container[0]).empty();
+		},
+		'render': function(data){
+			self.render(data);
+		}
+	};
+
+	self.render = function(data){
+		switch(options.mode){
+			case "eq":
+				self.renderEQ(data);
+				break;
+			case "circles":
+				self.renderCircles(data);
+				break;
+		}
+	};
+
+	self.renderEQ = function(data){
+		var eq = container.selectAll("g");
+
+		if (eq[0].length === 0){
+			eq
+				.data(base_data)
+				.enter()
+				.append("g");
+		}
+
+		eq.data(data).enter();
+
+		eq.append("rect")
+			.attr("x", function(d,i){return i*50 + 15;})
+			.attr("y", function(d,i){return d * 50;})
+			.attr("height", function(d,i){ return d*50;})
+			.attr("width", "15px")
+			.style("fill", function(d){
+				return "green";
+			});
+
+	};
+
+	self.renderCircles = function(data){
+		var self = this;
+		var circles = container.selectAll("circle");
+
+		if (circles[0].length === 0){
+			circles
+				.data(base_data)
+				.enter()
+				.append("circle");
+		}
+
+		circles.data(data).enter();
 
 		circles
 			.attr("cx", function(d,i){
@@ -73,8 +144,8 @@ $(document).ready(function(){
 			})
 			.style("fill", function(d){
 				return "green";
-			})
-			.transition()
-			.delay(10);
-	});
-})
+			});
+	};
+
+	return self.api;
+};
